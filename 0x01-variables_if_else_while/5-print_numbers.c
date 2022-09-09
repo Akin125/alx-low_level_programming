05324bd2f4e6a894033598c41cea2858d6216686
odediran philip <philipoluseyi@gmail.com>
2022-09-09 10:57:04 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0 - 9
+ *
+ * Description - prints in base 10 starting from 0
+ *
+ * Return : 0 on success
+ */
+int main(void)
+{
+printf("012345679\n");
+return (0);
+}
+

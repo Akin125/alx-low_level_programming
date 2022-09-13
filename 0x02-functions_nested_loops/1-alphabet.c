05aeb956059a43a793fe54e121123e7a6018589f
odediran philip <philipoluseyi@gmail.com>
2022-09-13 13:05:43 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * description - print main in lowercase
+ * Return: Nothing.
+ *
+ */
+
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+

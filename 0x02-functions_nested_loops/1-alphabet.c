cfe95c6c08899d95ac758c10d8d4b93dbebd9567
odediran philip <philipoluseyi@gmail.com>
2022-09-13 13:43:16 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,9 @@
 #include "main.h"
-
 /**
- * main - Entry point
- * description - print main in lowercase
- * Return: Nothing.
+ * print_alphabet - print in lowercase a - z
  *
+ * Return: Always 0 (Success).
  */
-
 void print_alphabet(void)
 {
 char c;

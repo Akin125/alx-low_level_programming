6e7dd07c0f81ffec3d28c2762a36ed49f764678f
odediran philip <philipoluseyi@gmail.com>
2022-09-13 22:19:48 -0700

    task 15

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 /**
- * main - print a table of the first 178 fibonacci numbers.
- * Return: Nothing.
+ * * main - Prints the first 98 Fibonacci numbers, starting with
+ *  1 and 2, separated by a comma followed by a space.
+ * Return: Always 0.
  */
 int main(void)
 {
 int count;
-unsigned long i, j, k;
-unsigned long ia, ib, ja, jb, ka, kb, carry;
-unsigned long amount;
-count = 0;
-i = 0;
-j = 1;
-amount = 10000000000000000000;/*10^18*/
-for (count = 1; count <= 91; count++)
+unsigned long fib1 = 0, fib2 = 1, sum;
+unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
+unsigned long half1, half2;
+for (count = 0; count < 92; count++)
 {
-k = i + j;
-i = j;
-j = k;
-printf("%d %lu\n", count + 1, k);
+sum = fib1 + fib2;
+printf("%lu, ", sum);
+fib1 = fib2;
+fib2 = sum;
 }
-ia = i / amount;
-ib = i % amount;
-ja = j / amount;
-jb = j % amount;
-while (count <= 178)
+fib1_half1 = fib1 / 10000000000;
+fib2_half1 = fib2 / 10000000000;
+fib1_half2 = fib1 % 10000000000;
+fib2_half2 = fib2 % 10000000000;
+for (count = 93; count < 99; count++)
 {
-carry = (ib + jb) / amount;
-ka = (ia + ja) +carry;
-kb = (ib + jb) -carry *amount;
-ia = ja;
-ib = jb;
-ja = ka;
-jb = kb;
-printf("%d %lu%018lu\n", count + 1, ka, kb);
-count++;
+half1 = fib1_half1 + fib2_half1;
+half2 = fib1_half2 + fib2_half2;
+if (fib1_half2 + fib2_half2 > 9999999999)
+{
+half1 += 1;
+half2 %= 10000000000;
 }
-printf("%lu018", ka);
+printf("%lu%lu", half1, half2);
+if (count != 98)
+printf(", ");
+fib1_half1 = fib2_half1;
+fib1_half2 = fib2_half2;
+fib2_half1 = half1;
+fib2_half2 = half2;
+}
+printf("\n");
 return (0);
 }
 

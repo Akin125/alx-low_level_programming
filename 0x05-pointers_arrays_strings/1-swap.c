188db8222eca319025929617453b67792aa108a2
odediran philip <philipoluseyi@gmail.com>
2022-09-19 04:59:01 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps values bnof two int
+ * @a: value for first int
+ * @b: value of second int
+ */
+
+void swap_int(int *a, int *b)
+{
+	int sey;
+
+	sey = *a;
+	*a = *b;
+	*b = sey;
+}
+

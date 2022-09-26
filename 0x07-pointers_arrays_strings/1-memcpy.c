ab9118bce301df1f9f99a04e7ed6db4633db715e
odediran philip <philipoluseyi@gmail.com>
2022-09-26 07:49:14 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,13 +11,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
-
-	for (index = 0; index < n; index++)
-	destination[index] = source[index];
-
-	return (dest);
+unsigned int  i;
+i = 0;
+while (i < n)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
 }
 
